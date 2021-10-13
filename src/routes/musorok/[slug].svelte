fb3881aeab2d99027068fb125112360ce30a77a3
rudywaltz <david.barkoczi@gmail.com>
2021-10-13 22:50:39 +0200

    fix: podcast variable

--- src/routes/musorok/[slug].svelte
+++ src/routes/musorok/[slug].svelte
@@ -26,9 +26,9 @@
     if (response.ok) {
       const podcastFeed = await response.text()
       let podcastJSON = JSON.parse(podcastFeed)[0]
-      poscastJSON.logoUrl = poscastJSON.logoUrl?.startsWith('http://') ?
-        `${API_ENDPOINT}/image?url=${poscastJSON.logoUrl}`:
-        poscastJSON.logoUrl
+      podcastJSON.logoUrl = podcastJSON.logoUrl?.startsWith('http://') ?
+        `${API_ENDPOINT}/image?url=${podcastJSON.logoUrl}`:
+        podcastJSON.logoUrl
 
       podcastJSON.shows = podcastJSON.shows.map(show => {
         show.image = show.image?.startsWith('http://') ? `${API_ENDPOINT}/image?url=${show.image}`: show.image
